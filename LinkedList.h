ae2622c339a1e563af4a25668a1b0db1dd3e8e93
Devin Piner <devpindroid@gmail.com>
2018-11-15 22:18:15 -0700

    data type is now an integer

---
+++ LinkedList.h
@@ -0,0 +1,65 @@
+// Modified By: Devin Piner
+// Filename: LinkedList.h
+// Description: Interface of the LinkedList class
+// Last Updated: October 30, 2018
+// Added iterator class
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "Node.h"
+
+class LLIterator;
+typedef int T;
+
+class LinkedList {
+public:
+    //required functions
+    LinkedList( void ) = default;
+    LinkedList( const LinkedList& );
+    ~LinkedList( void );
+    bool insertAtFront( const T& );
+    bool insertAtBack( const T& );
+    bool deleteAtFront( void );
+    inline int length( void ) { return size; }
+    bool deleteAtBack( void );
+    bool insertBeforePosition( int, const T& );
+    bool deleteBeforePosition( int );
+
+
+    //helper functions
+    void clear( void );
+    inline bool empty() const { return size == 0; }
+    void printVals( void ) const ;
+
+    class LLIterator {
+    public:
+        // TODO DO THE MOV OP
+        LLIterator( ) = default;
+        explicit LLIterator( Node* n ) { ptr = n; };
+        LLIterator( const LLIterator& l ) { ptr = l.ptr; };
+        LLIterator& operator=( const LLIterator& l ) {
+            if ( &l == this ) {
+                return *this;
+            }
+            ptr = l.ptr;
+            return *this;
+        };
+        const int& operator*( ) const { if (  ptr != nullptr ) return ptr->data; };
+        int& operator*( ) { if (  ptr != nullptr ) return ptr->data; };
+        void operator++( ) { if (  ptr != nullptr ) ptr = ptr->next; };
+        bool operator!=( const LLIterator &l ) { return ptr != l.ptr;  };
+    private:
+        Node* ptr = nullptr;
+    };
+
+    LLIterator begin( void ) const { return LLIterator(headPtr); };
+    LLIterator end( void ) const { return LLIterator(nullptr); };
+
+private:
+    Node* headPtr = nullptr;
+    unsigned int size = 0;
+};
+
+
+#endif //LINKEDLIST_H
---
+++ Node.h
@@ -0,0 +1,14 @@
+// Author: Devin Piner
+// Filename: Node.h
+// Description: Interface of the Node class
+// Last Updated: February 20, 2017
+
+#ifndef HW5_NODE_H
+#define HW5_NODE_H
+
+struct Node {
+    int data;
+    Node* next = nullptr;
+};
+
+#endif //HW5_NODE_H
